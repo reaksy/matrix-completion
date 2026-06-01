be5f6beda0bb8bd3405cb99f0c4d15b9486f5664
reaksy <karim.alyautdinov@mail.ru>
2026-06-01 15:38:29 +0300

    Trim repository to library API and notebook

--- progress2/build_part0_pdf.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-set -euo pipefail
-
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-cd "$SCRIPT_DIR"
-
-/usr/local/texlive/2026/bin/universal-darwin/pdflatex -interaction=nonstopmode -file-line-error part0_initial_experiments.tex
-/usr/local/texlive/2026/bin/universal-darwin/pdflatex -interaction=nonstopmode -file-line-error part0_initial_experiments.tex
